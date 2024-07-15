7d28293bcf2d4639e2cdb252b0519ca3b308efc5
Hermloth <herm_loth@hotmail.com>
2024-07-16 00:32:12 +1000

    Addbasic sidebar layout and color scheme

--- src/LayoutBuilder.js
+++ src/LayoutBuilder.js
@@ -3,12 +3,34 @@ console.log("Build Layout Loaded")
 
 const body = document.body;
 
+const testclass = "TestClass";
+
+
 const parentDiv = document.createElement("div");
 parentDiv.classList.add("parentDiv");
-parentDiv.textContent = "Sample Parent Div";
-
 document.body.appendChild(parentDiv);
 
+const sideBarContainer = document.createElement("div");
+sideBarContainer.classList.add("Sidebar");
+parentDiv.appendChild(sideBarContainer);
+
+const sideBarTitleContainer = document.createElement("div");
+sideBarTitleContainer.classList.add("TitleContainer");
+sideBarContainer.appendChild(sideBarTitleContainer);
+
+const sideBarIconContainer = document.createElement("div");
+sideBarIconContainer.classList.add("DashboardIcon");
+sideBarIconContainer.textContent = "test icon"
+sideBarTitleContainer.appendChild(sideBarIconContainer);
+
+const sideBarHeadingContainer = document.createElement("div");
+sideBarHeadingContainer.classList.add("DashboardTitle");
+sideBarTitleContainer.appendChild(sideBarHeadingContainer);
+
+const dashboardTitle = document.createElement("h1");
+dashboardTitle.classList.add("Title");
+dashboardTitle.textContent = "Dashboard";
+sideBarHeadingContainer.appendChild(dashboardTitle);
 
 
 
@@ -19,6 +41,10 @@ document.body.appendChild(parentDiv);
 
 
 
+const footer = document.createElement("div");
+footer.classList.add("Footer")
+footer.textContent = "This is not a real website, this is a project assignment for the Odin Project. Author: Hermloth"
+parentDiv.appendChild(footer);
 
 
 }
