f5b84d3c9fa95a6f3108a3e2867586fd05b7a4e4
Hermloth <herm_loth@hotmail.com>
2024-07-24 21:18:42 +1000

    finish navigation bar structure

--- src/LayoutBuilder.js
+++ src/LayoutBuilder.js
@@ -1,3 +1,13 @@
+import HomeIcon from './assets/home.svg';
+import AccountIcon from './assets/card-account-details.svg';
+import MessageIcon from './assets/message.svg';
+import HistoryIcon from './assets/history.svg';
+import TaskIcon from './assets/note-multiple.svg';
+import CommunitiesIcon from './assets/account-group.svg';
+import SettingsIcon from './assets/cog.svg';
+import HelpIcon from './assets/help-box.svg';
+import PrivacyIcon from './assets/shield-check.svg';
+
 export function BuildLayout () {
 	console.log("Build Layout Loaded");
 
@@ -35,4 +45,62 @@ export function BuildLayout () {
 	footer.classList.add("Footer")
 	footer.textContent = `This is not a real website, this is a project assignment for the Odin Project. Author: Hermloth - Webpack: ${process.env.IS_PRODUCTION} ${GLOBAL_TEST}`
 	parentDiv.appendChild(footer);
+
+	const menuListContainer = document.createElement("div");
+	menuListContainer.classList.add("MenuListContainer");
+
+
+	const adminMenuListContainer = document.createElement("div");
+	adminMenuListContainer.classList.add("MenuListContainer");
+	var menuItemsText = ["Home","Profile","Message","History","Tasks","Communities"];
+	var menuItemsIcons = [HomeIcon, AccountIcon,MessageIcon,HistoryIcon,TaskIcon,CommunitiesIcon];
+	var adminMenuItemsText = ["Settings","Support","Privacy"];
+	var adminMenuIcons = [SettingsIcon, HelpIcon, PrivacyIcon];
+
+	for (let menuitem in menuItemsText){
+		let container = document.createElement("div")
+		container.classList.add("menuContainerRow");
+
+		let menuRowIcon = document.createElement("img");
+		menuRowIcon.classList.add("menuRowIcon");
+		menuRowIcon.src = menuItemsIcons[menuitem];
+		container.appendChild(menuRowIcon);
+
+		let menuRowText = document.createElement("div");
+		menuRowText.classList.add("menuRowText");
+		menuRowText.textContent = menuItemsText[menuitem];
+		container.appendChild(menuRowText);
+
+		menuListContainer.appendChild(container);
+		console.log(menuItemsText[menuitem]);
+	}
+	
+	for (let adminMenuItem in adminMenuItemsText){
+		let container = document.createElement("div");
+		container.classList.add('menuContainerRow');
+
+		let menuRowIcon = document.createElement("img");
+		menuRowIcon.classList.add("menuRowIcon");
+		menuRowIcon.src = adminMenuIcons[adminMenuItem];
+		container.appendChild(menuRowIcon);
+
+		let adminMenuText = document.createElement("div");
+		adminMenuText.classList.add("menuRowText");
+		adminMenuText.textContent = adminMenuItemsText[adminMenuItem];
+
+		container.appendChild(adminMenuText);
+		adminMenuListContainer.appendChild(container);
+	}
+
+	sideBarContainer.appendChild(menuListContainer);
+	sideBarContainer.appendChild(adminMenuListContainer);
+	
+
+
+
+
+
+
+
+
 }
