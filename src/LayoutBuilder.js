ba2daa2c7ba587e7a7087cbbcce9044383342706
Matthew Labrum <matthew.labrum@lynkz.com.au>
2024-07-24 15:50:32 +0930

    Fixes

--- src/LayoutBuilder.js
+++ src/LayoutBuilder.js
@@ -1,53 +1,38 @@
-function BuildLayout () {
-console.log("Build Layout Loaded");
+export function BuildLayout () {
+	console.log("Build Layout Loaded");
+
+	const body = document.body;
+
+	const testclass = "TestClass";
+
+	const parentDiv = document.createElement("div");
+	parentDiv.classList.add("parentDiv");
+	document.body.appendChild(parentDiv);
+
+	const sideBarContainer = document.createElement("div");
+	sideBarContainer.classList.add("Sidebar");
+	parentDiv.appendChild(sideBarContainer);
+
+	const sideBarTitleContainer = document.createElement("div");
+	sideBarTitleContainer.classList.add("TitleContainer");
+	sideBarContainer.appendChild(sideBarTitleContainer);
+
+	const sideBarIconContainer = document.createElement("div");
+	sideBarIconContainer.classList.add("DashboardIcon");
+	sideBarTitleContainer.appendChild(sideBarIconContainer);
 
 
-const body = document.body;
+	const sideBarHeadingContainer = document.createElement("div");
+	sideBarHeadingContainer.classList.add("DashboardTitle");
+	sideBarTitleContainer.appendChild(sideBarHeadingContainer);
 
-const testclass = "TestClass";
+	const dashboardTitle = document.createElement("h1");
+	dashboardTitle.classList.add("Title");
+	dashboardTitle.textContent = "Dashboard";
+	sideBarHeadingContainer.appendChild(dashboardTitle);
 
-
-const parentDiv = document.createElement("div");
-parentDiv.classList.add("parentDiv");
-document.body.appendChild(parentDiv);
-
-const sideBarContainer = document.createElement("div");
-sideBarContainer.classList.add("Sidebar");
-parentDiv.appendChild(sideBarContainer);
-
-const sideBarTitleContainer = document.createElement("div");
-sideBarTitleContainer.classList.add("TitleContainer");
-sideBarContainer.appendChild(sideBarTitleContainer);
-
-const sideBarIconContainer = document.createElement("div");
-sideBarIconContainer.classList.add("DashboardIcon");
-sideBarTitleContainer.appendChild(sideBarIconContainer);
-
-
-const sideBarHeadingContainer = document.createElement("div");
-sideBarHeadingContainer.classList.add("DashboardTitle");
-sideBarTitleContainer.appendChild(sideBarHeadingContainer);
-
-const dashboardTitle = document.createElement("h1");
-dashboardTitle.classList.add("Title");
-dashboardTitle.textContent = "Dashboard";
-sideBarHeadingContainer.appendChild(dashboardTitle);
-
-
-
-
-
-
-
-
-
-
-const footer = document.createElement("div");
-footer.classList.add("Footer")
-footer.textContent = "This is not a real website, this is a project assignment for the Odin Project. Author: Hermloth"
-parentDiv.appendChild(footer);
-
-
+	const footer = document.createElement("div");
+	footer.classList.add("Footer")
+	footer.textContent = `This is not a real website, this is a project assignment for the Odin Project. Author: Hermloth - Webpack: ${process.env.IS_PRODUCTION} ${GLOBAL_TEST}`
+	parentDiv.appendChild(footer);
 }
-
-module.exports = BuildLayout;
--- src/javascript.js
+++ src/javascript.js
@@ -1,5 +1,5 @@
 import './style.css';
-const BuildLayout = require('./LayoutBuilder');
+import {BuildLayout} from './LayoutBuilder'
 import Icon from './view-dashboard.svg';
 
 BuildLayout();
